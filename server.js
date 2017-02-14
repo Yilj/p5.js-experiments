1c7e781ff2959b3d8c916073e82afa6bbd622f49
Yilj <nigk@web.de>
2017-02-14 19:55:49 +0100

    Adding express to server

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
+var express = require('express');
+var expressApp = express();
+var expressServer = expressApp.listen(3000);
+
+expressApp.use(express.static('public'));
+
 console.log("node.js server running ...");
