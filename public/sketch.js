fad7b860785f4e08a7403b6b12390bc4acfb206f
Yilj <nigk@web.de>
2017-02-14 21:21:25 +0100

    Adding simple socket compatiblity

--- public/sketch.js
+++ public/sketch.js
@@ -1,7 +1,17 @@
+var socket;
+
 function setup() {
     createCanvas(600, 600);
+    background(51);
+
+    socket = io.connect('http://nodejs-experiments-p5js-experiments.44fs.preview.openshiftapps.com/');
+    socket.on('dataToClient', dataFromServer);
 }
 
-function draw() {
-    background(51);
+function sendData(data) {
+    socket.emit('dataToServer', data);
+}
+
+function dataFromServer(data) {
+    console.log(data));
 }
--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+//setting up express
 var express = require('express');
 var expressApp = express();
+//setting up an express server on port 8080
 var expressServer = expressApp.listen(8080);
-
 expressApp.use(express.static('public'));
 
+//setting up socket
+var socket = require('socket.io');
+var io = socket(expressServer);
+
 console.log("node.js server running ...");
+
+
+io.sockets.on('connection', newSocketConnection);
+
+function newSocketConnection(socket) {
+    console.log('new socket connection: "' + socket.id + '"');
+
+    socket.on('dataToServer', dataFromClient);
+
+    function dataFromClient(data) {
+        socket.broadcast.emit('dataToClient', data);
+        console.log(data);
+    }
+}
