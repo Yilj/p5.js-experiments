ed40098d18fde6b8fe5143e282982c5cee767683
Yilj <nigk@web.de>
2017-02-14 20:48:42 +0100

    Updating server to use expamle code

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var express = require('express');
-var expressApp = express();
-var expressServer = expressApp.listen(8080);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-expressApp.use(express.static('public'));
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
 
 console.log("node.js server running ...");
