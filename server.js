959e5348c0da1668eca89bfadf8913e0e336367c
Yilj <nigk@web.de>
2017-02-14 21:00:35 +0100

    Downdating Server to using express

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var express = require('express');
+var expressApp = express();
+var expressServer = expressApp.listen(8080);
 
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
-});
+expressApp.use(express.static('public'));
 
 console.log("node.js server running ...");
