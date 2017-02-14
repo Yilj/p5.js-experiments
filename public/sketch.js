f910bd4b215055de2f7187dbf6cd263af5d9a137
Yilj <nigk@web.de>
2017-02-14 21:56:49 +0100

    Adding a sketch for online drawing

--- public/sketch.js
+++ public/sketch.js
@@ -14,4 +14,24 @@ function sendData(data) {
 
 function dataFromServer(data) {
     console.log(data);
+    drawData(data, false);
+}
+
+function mouseDragged() {
+    var data = {
+        mouseX,
+        mouseY
+    }
+    sendData(data);
+    drawData(data, true);
+}
+
+function drawData(data, own) {
+    if (own) {
+      fill(150, 10, 100, 5);
+    } else {
+        fill(150, 10, 10, 5);
+    }
+    noStroke();
+    ellipse(data.mouseX, data.mouseY, 20, 20);
 }
