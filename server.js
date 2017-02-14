628778f92fed0fc308437e21f51cd801fc058e33
Yilj <nigk@web.de>
2017-02-14 20:44:16 +0100

    Updating server to use port 8080

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var expressApp = express();
-var expressServer = expressApp.listen(3000);
+var expressServer = expressApp.listen(8080);
 
 expressApp.use(express.static('public'));
 
