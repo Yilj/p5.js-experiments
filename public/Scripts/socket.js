1aa3b22aa68a94f0d0766274946e6c47674ee390
Yilj <nigk@web.de>
2017-02-16 14:51:00 +0100

    correcting to actual server url

--- public/Scripts/socket.js
+++ public/Scripts/socket.js
@@ -1,8 +1,8 @@
 //socket object for managing socket server communication
 function socket() {
     //socket server url
-    //this.url = 'http://nodejs-experiments-p5js-experiments.44fs.preview.openshiftapps.com/';
-    this.url = 'http://localhost:8080';
+    this.url = 'http://nodejs-experiments-p5js-experiments.44fs.preview.openshiftapps.com/';
+    //this.url = 'http://localhost:8080';
     //connecting to socket server with url
     this.socket = io.connect(this.url);
     //setting up function called when a message is received from server
